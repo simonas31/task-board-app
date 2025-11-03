2c35453d41125fb55afc305c3629214a0a8242e7
simonas31 <babarskas.simonas@gmail.com>
2025-11-03 16:38:40 +0200

    fixed some docker errors and added couple of ui components

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
     host: true,
     port: 3000,
     strictPort: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
