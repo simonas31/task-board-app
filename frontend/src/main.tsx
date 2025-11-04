f9bbfb1a928423265a43e59b0fca3923b9acbc1f
simonas31 <babarskas.simonas@gmail.com>
2025-11-04 13:00:02 +0200

    login page done

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import { Toaster } from "@/components/ui/sonner";
-import App from "./App.tsx";
 import "./styles/globals.css";
+import { RoutesMap } from "./routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
-        <App />
+        <RoutesMap />
         <Toaster />
       </ThemeProvider>
     </BrowserRouter>
--- frontend/src/pages/login-page.tsx
+++ frontend/src/pages/login-page.tsx
@@ -1,5 +1,92 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Lock, User } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const loginSchema = z.object({
+  email: z.email(),
+  password: z.string().min(1, {
+    message: "Password is required",
+  }),
+});
+
 const LoginPage = () => {
-  return <>Login page</>;
+  const form = useForm<z.infer<typeof loginSchema>>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  function onSubmit(values: z.infer<typeof loginSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Card className="w-md mx-3 sm:mx-auto py-5 px-5 sm:px-12">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+            <div className="text-center">
+              <h4>Login</h4>
+              <p className="mt-2 text-muted-foreground">
+                Enter your credentials to continue
+              </p>
+            </div>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <InputGroup>
+                      <InputGroupInput {...field} />
+                      <InputGroupAddon>
+                        <User />
+                      </InputGroupAddon>
+                    </InputGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <InputGroup>
+                      <InputGroupInput type="password" {...field} />
+                      <InputGroupAddon>
+                        <Lock />
+                      </InputGroupAddon>
+                    </InputGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="w-full mt-2">Sign in</Button>
+          </form>
+        </Form>
+      </Card>
+    </>
+  );
 };
 
 export default LoginPage;
