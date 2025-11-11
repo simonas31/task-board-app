1941ef0a2eb1cda9d66c1ddf1b57a1424793246f
simonas31 <babarskas.simonas@gmail.com>
2025-11-11 18:50:58 +0200

    list projects page remove row on delete

--- frontend/src/pages/projects/list-projects-page.tsx
+++ frontend/src/pages/projects/list-projects-page.tsx
@@ -7,6 +7,7 @@ import {
 import { api } from "@/lib/axios";
 import type { ColumnDef } from "@tanstack/react-table";
 import { Edit, Plus, Trash } from "lucide-react";
+import * as React from "react";
 import { Link } from "react-router";
 import { toast } from "sonner";
 import useSWR from "swr";
@@ -20,71 +21,79 @@ type Project = {
 
 const fetcher = (url: string) => api.get(url).then((res) => res.data.projects);
 
-const columns: ColumnDef<Project>[] = [
-  {
-    accessorKey: "id",
-    id: "select",
-    size: 30,
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-    enableResizing: false,
-  },
-  {
-    accessorKey: "name",
-    header: "Project Name",
-  },
-  {
-    id: "actions",
-    header: "Actions",
-    size: 120,
-    cell: ({ row }) => {
-      const rowId = row.getValue("select");
-      return (
-        <div className="flex items-center space-x-2">
-          <Link to={`/projects/${rowId}/edit`}>
-            <Button size="sm">
-              <Edit />
-            </Button>
-          </Link>
-          <Button
-            variant="destructive"
-            size="sm"
-            onClick={() => {
-              api.delete(`/projects/${rowId}`).then(() => {
-                toast.success("Project was deleted successfully!");
-              });
-            }}
-          >
-            <Trash />
-          </Button>
-        </div>
-      );
-    },
-  },
-];
-
 export default function ListProjectsPage() {
-  const { data, isLoading } = useSWR<DataTableResponseData<Project>>(
+  const { data: swrData, isLoading } = useSWR<DataTableResponseData<Project>>(
     "/projects",
     fetcher
   );
+  const [data, setData] = React.useState(() => swrData?.data || []);
+
+  const columns = React.useMemo<ColumnDef<Project>[]>(() => {
+    return [
+      {
+        accessorKey: "id",
+        id: "select",
+        size: 30,
+        header: ({ table }) => (
+          <Checkbox
+            checked={
+              table.getIsAllPageRowsSelected() ||
+              (table.getIsSomePageRowsSelected() && "indeterminate")
+            }
+            onCheckedChange={(value) =>
+              table.toggleAllPageRowsSelected(!!value)
+            }
+            aria-label="Select all"
+          />
+        ),
+        cell: ({ row }) => (
+          <Checkbox
+            checked={row.getIsSelected()}
+            onCheckedChange={(value) => row.toggleSelected(!!value)}
+            aria-label="Select row"
+          />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+        enableResizing: false,
+      },
+      {
+        accessorKey: "name",
+        header: "Project Name",
+      },
+      {
+        id: "actions",
+        header: "Actions",
+        size: 120,
+        cell: ({ row }) => {
+          const rowId = row.getValue("select");
+          return (
+            <div className="flex items-center space-x-2">
+              <Link to={`/projects/${rowId}/edit`}>
+                <Button size="sm">
+                  <Edit />
+                </Button>
+              </Link>
+              <Button
+                variant="destructive"
+                size="sm"
+                onClick={() => {
+                  api.delete(`/projects/${rowId}`).then(() => {
+                    toast.success("Project was deleted successfully!");
+                    const dataCopy = [...data];
+                    dataCopy.splice(row.index, 1);
+                    setData(dataCopy);
+                  });
+                }}
+              >
+                <Trash />
+              </Button>
+            </div>
+          );
+        },
+      },
+    ];
+  }, [data]);
 
   return (
     <div className="container mx-auto">
@@ -95,11 +104,7 @@ export default function ListProjectsPage() {
           </Button>
         </Link>
       </div>
-      <DataTable
-        columns={columns}
-        data={data?.data || []}
-        isLoading={isLoading}
-      />
+      <DataTable columns={columns} data={data} isLoading={isLoading} />
     </div>
   );
 }
